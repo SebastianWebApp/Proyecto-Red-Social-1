0b830aee732a6da61d0852124d57f4da7054a92b
SebastianWebApp <mateoespinosam@hotmail.com>
2024-12-09 20:26:49 -0500

    Reconexion de base de datos mongodb

--- Microservicios/Verificacion_2Pasos/Database/mongodb.js
+++ Microservicios/Verificacion_2Pasos/Database/mongodb.js
@@ -3,6 +3,10 @@ import mongoose from "mongoose";
 const connectToDB = async () => {
     await mongoose.connect(process.env.MONGODB).then( res => {
         console.log("Base de datos conectada");
+    }).catch(async error => {
+        await new Promise(resolve => setTimeout(resolve, 10000)); // Espera 10 segundos antes de reintentar
+        console.log("Reintentando conectar a la base de datos...");
+        await connectToDB();
     });
 }
 
